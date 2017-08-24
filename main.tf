c15af6eeb35c2305001b37f1679e16750a5746dd
Brett Wright <brett.wright@bambora.com>
2017-08-24 10:28:58 +1000

    Updated S3 bucket name

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
-variable "bucket" { default = "btw-blog"}
-variable "bucket_content" { default = "myblog/public" }
+variable "bucket" { default = "myblog.btwsolutions.net"}
+
 
 resource "aws_s3_bucket" "blog" {
-    bucket  = "${var.bucket}"
-    acl     = "public-read"
+    bucket      = "${var.bucket}"
+    acl         = "public-read"
 
     website {
         index_document  = "index.html"
+        error_document  = "404.html"
     }
+}
+
+resource "aws_route53_record" "blog" {
+    zone_id     = "${var.route53id}"
+    name        = "myblog.btwsolutions.net" 
+    type        = "CNAME"
+    ttl         = "300"
+    records     = ["${aws_s3_bucket.blog.website_endpoint}"]
+
 }
